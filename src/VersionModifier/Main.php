8fc8cc1b45a8aa5bc27ee15cd6318cfc5d819c34
Keith <keithkfng@users.noreply.github.com>
2016-04-24 07:54:17 +1000

    Update and rename src/HelpModifier/Main.php to src/VersionModifier/Main.php

---
+++ src/VersionModifier/Main.php
@@ -0,0 +1,45 @@
+<?php
+
+  namespace VersionModifier;
+
+  use pocketmine\plugin\PluginBase;
+  use pocketmine\event\Listener;
+  use pocketmine\utils\TextFormat as TF;
+  use pocketmine\utils\Config;
+  use pocketmine\event\player\PlayerCommandPreprocessEvent;
+
+  class Main extends PluginBase implements Listener {
+
+    public function dataPath() {
+
+      return $this->getDataFolder();
+
+    }
+
+    public function onEnable() {
+
+      $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+      @mkdir($this->dataPath());
+
+      $this->cfg = new Config($this->dataPath() . "config.yml", Config::YAML, array("page_1" => array("message_1" => "This server is running ImagicalMine 1.4")));
+
+    }
+
+    public function sendHelp(PlayerCommandPreprocessEvent $event) {
+
+      $command = explode(" ", strtolower($event->getMessage()));
+
+      $player = $event->getPlayer();
+
+      if($command[0] === "/version") {
+
+          $page_one_messages = $this->cfg->get("page_1");
+
+          $player->sendMessage($page_one_messages["message_1"]);
+
+          $event->setCancelled();
+
+        }
+
+      }
